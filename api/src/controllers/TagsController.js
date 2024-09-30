7e6663c9a482e66bbd33909fc42a66733ca232c3
Zonary <128634065+xXZonaryXx@users.noreply.github.com>
2024-09-30 20:02:09 -0300

    Add files via upload

---
+++ api/src/controllers/TagsController.js
@@ -0,0 +1,17 @@
+const knex = require('../database/knex');
+
+class TagsController{
+    async index(request, response){
+        const user_id = request.user.id;
+
+        const tags = await knex('tags')
+           .where({ user_id })
+           .groupBy("name")
+           .orderBy(knex.raw('LOWER(name)'), "asc");
+           
+        return response.json(tags);
+    }
+
+}
+
+module.exports = TagsController;
---
+++ api/src/routes/users.routes.js
@@ -0,0 +1,19 @@
+const { Router, response } = require("express");
+const multer = require("multer");
+const uploadCongfig = require("../configs/upload");
+
+const UsersController = require("../controllers/UsersController");
+const UserAvatarController = require("../controllers/UserAvatarController");
+const ensureAuthenticated = require("../middlewares/ensureAuthenticated");
+
+const usersRoutes = Router();
+const upload = multer(uploadCongfig.MULTER);
+
+const usersController = new UsersController();
+const userAvatarController = new UserAvatarController();
+
+usersRoutes.post("/", usersController.create);
+usersRoutes.put("/", ensureAuthenticated, usersController.update);
+usersRoutes.patch("/avatar", ensureAuthenticated, upload.single("avatar"), userAvatarController.update);
+
+module.exports = usersRoutes;
